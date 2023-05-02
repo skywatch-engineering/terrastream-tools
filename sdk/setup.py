eaeee95d0f8b0c319ce65bb77c56c76b623eb219
Paulo Cruz <paulo@skywatch.com>
2023-05-02 11:08:47 -0400

    add license headers to all files

--- sdk/setup.py
+++ sdk/setup.py
@@ -1,22 +1,22 @@
+# Copyright 2023 Skywatch Space Applciations Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 from os import path
 from shutil import rmtree
 
 from setuptools import setup
 
 module_name = "ts_sdk"
-
-
-def cleanup(name):
-    if path.exists("dist"):
-        rmtree("dist")
-    if path.exists("build"):
-        rmtree("build")
-    if path.exists(f"{name}.egg-info"):
-        rmtree(f"{name}.egg-info")
-
-
-cleanup(module_name)
-
 
 setup(
     name=module_name,  # This is the name of your PyPI-package.
